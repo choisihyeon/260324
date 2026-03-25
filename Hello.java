1168620fab0f4e4f40e89a459e476ab29de3e19d
choisihyeon <csin105@bu.ac.kr>
2026-03-25 18:15:37 +0900

    주석 내용 추가

--- Hello.java
+++ Hello.java
@@ -12,10 +12,11 @@ public class Hello {
 		System.out.println("Hello"+name);
 	}
 
+	// 진입 메서드
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Hello hello  =new Hello();
-		hello.print("Java");
+		Hello hello  =new Hello(); //new 키워드를 사용해 객체 생성
+		hello.print("Javaa"); 
 	}
 
 }
